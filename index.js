a81da2b1c7eda2ca70a5c1dfa712479291d176c9
Aftab Ali <aftab.a.sikander@gmail.com>
2018-04-03 23:45:25 +0500

    updating package

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
-import Header from './src/components/header';
+import {AppRegistry, View} from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => {
     const props = {headerHeading: 'Hello New Albums from root', headerSubheading: "sub heading"};
     return (
-        // Passing multiple Props inside a components Way #1
-        //<Header {...props}/>
+        <View>
+            // Passing multiple Props inside a components Way #1
+            //<Header {...props}/>
 
-        // Passing multiple Props inside a components Way #2
-        <Header headerHeading="Hello New Albums from root" headerSubheading="sub heading"/>
+            // Passing multiple Props inside a components Way #2
+            <Header headerHeading="Hello New Albums from root" headerSubheading="sub heading"/>
+
+            <AlbumList/>
+
+        </View>
+
     );
 };
 
